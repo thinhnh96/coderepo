edc659faaeebccca5ade9e8909d24015b87ede2f
thinhnh96 <161121686+thinhnh96@users.noreply.github.com>
2026-01-21 21:35:23 +0700

    Update app.js
    update CI

--- scr/app.js
+++ scr/app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-// Health check 
+// Health check run
 app.get('/health', (req, res) => {
   res.status(200).json({
     status: 'OK',
